1b3b714840a30abd4cf9d22a891b869c00099f04
Mathew <mwakre29@gmail.com>
2020-08-30 19:37:47 -0700

    Add Splash Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Splash from "./components/Splash/index";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path='/' component={Splash} />
+      </Switch>
     </div>
   );
 }
